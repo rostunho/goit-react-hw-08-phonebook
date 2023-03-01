dea74f5c577e15ccfd096032daf0d529f1bdca43
Rostyk Podolets <ros.podolets@gmail.com>
2023-03-01 10:15:59 +0200

    Layout and Sidebar components edited

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,18 @@
 import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
 import { Logo } from 'components/Logo/Logo';
 import { UserMenu } from 'components/UserMenu/UserMenu';
 import { SideBar } from 'components/SideBar/SideBar';
 import { Header, Container } from './Layout.styled';
+import { useAuth } from 'hooks/useAuth';
 
 export function Layout() {
-  const userIsLoggedIn = useSelector(selectIsLoggedIn);
+  const { isLoggedIn } = useAuth();
 
   return (
     <>
       <Header>
         <Logo />
-        {userIsLoggedIn && <UserMenu />}
+        {isLoggedIn && <UserMenu />}
       </Header>
       <Container>
         <SideBar />
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,15 +1,17 @@
 import { Navigation } from 'components/Navigation/Navigation';
 import { Filter } from 'components/Filter/Filter';
 
-// import { useAuth } from 'hooks/useAuth';
+import { useAuth } from 'hooks/useAuth';
 import { Container } from './SideBar.styled';
 import { ThemeSwitcher } from 'components/ThemeSwitcher/ThemeSwitcher';
 
 export function SideBar() {
+  const { isLoggedIn } = useAuth();
+
   return (
     <Container>
       <Navigation />
-      <Filter />
+      {isLoggedIn && <Filter />}
       <ThemeSwitcher />
     </Container>
   );
